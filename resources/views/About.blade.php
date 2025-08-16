a47767f893d32fbb0abc332f7d02796d2f85488c
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-08-17 00:15:57 +0530

    new one

---
+++ resources/views/About.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="{{ asset('main.css') }}">
+    <script src="{{ asset('main.js') }}"></script>
+</head>
+<body>
+    <div>
+        <h1>Here is your about page</h1>
+    </div>
+</body>
+</html>
