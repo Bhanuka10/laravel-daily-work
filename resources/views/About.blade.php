a236573f813a6382c649d19d9be4835332ac48e2
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-08-18 01:36:47 +0530

    COMPLETE today learning

--- resources/views/About.blade.php
+++ resources/views/About.blade.php
@@ -4,12 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us</title>
-    <link rel="stylesheet" href="{{ asset('main.css') }}">
-    <script src="{{ asset('main.js') }}"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+    
 </head>
 <body>
-    <div>
-        <h1>Here is your about page</h1>
+    <div class="container ">
+        <div class="text-center ">
+            <h1>Daily Tasks</h1>
+            <div class="row">
+                <div class="col-md-6 mx-auto">
+                   <input type="text" class="form-control" placeholder="Enter your Task" >
+                   
+                     <button class="btn btn-primary" type="button " value="SAVE">SAVE</button>
+                    <button class="btn btn-warning" type="button" value="CLEAR">CLEAR</button>
+
+            
+            <table class="table table-dark table-striped">
+                <th> ID</th>
+                <th>Task</th>
+                <th>completed</th>
+                <tr>
+                    <td>1</td>
+                    <td>i have learnt laraval today</td>
+                    <td>not yet</td>
+                </tr>
+  
+</table>
+</div>
+            </div>
+        </div>        
     </div>
 </body>
 </html>
